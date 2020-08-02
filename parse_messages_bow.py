b544842b68d71cf837bdffea38417ea025d92ea7
Marley <marleyxiong0@gmail.com>
2020-08-02 17:34:02 -0400

    updated parser with preprocessing

--- parse_messages_bow.py
+++ parse_messages_bow.py
@@ -26,7 +26,12 @@ Created on Fri May 29 21:18:35 2020
 import json
 import os
 from collections import Counter
-
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+# import nltk
+# nltk.download("stopwords")
+import re
+tokenizer = RegexpTokenizer('[a-zA-Z]\w+\'?\w*')
 # import nltk
 # from nltk.corpus import stopwords
 # stopwords.words('english')
@@ -34,12 +39,22 @@ from collections import Counter
 """
 Change the sender name and path to point to your download
 """
-sender_name = "Stephen Fay"
-path = 'data/messages/inbox/'
+sender_name = "Marley Xiong"
+path = 'messages-2018-2020/inbox/'
 folder_names = os.listdir(path)
 min_bytes = 1000
 selected_folders = []
 results = {}
+
+en_stopwords = set(stopwords.words('english'))
+def preprocess(s):
+    # preprocess a line of text
+    s = s.lower()
+    url_re = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
+    s = re.sub(url_re, "", s)
+    words = tokenizer.tokenize(s)
+    words = [w for w in words if w not in en_stopwords]
+    return words
 
 def parse_single_person(path):
     with open(path) as f:
@@ -48,12 +63,11 @@ def parse_single_person(path):
             person_name = data['participants'][0]['name']
             if person_name == sender_name:
                 person_name = data['participants'][1]['name']
-            print(person_name)
             word_counts = Counter()
             word_counts_self = Counter()
             for msg in data['messages']:
                 if (msg['type'] == "Generic" and 'content' in msg):
-                    count = Counter(msg['content'].split())
+                    count = Counter(preprocess(msg['content'].encode('latin1').decode('utf8')))
                     if msg['sender_name'] == person_name:
                         word_counts += count
                     else :
@@ -62,7 +76,7 @@ def parse_single_person(path):
             results[person_name] = {'word_counts': dict(word_counts), 'word_counts_self': dict(word_counts_self)}
             
 for f in folder_names: 
-    if f != '.DS_Store' and f.lower()==f:
+    if f != '.DS_Store':
         size = os.path.getsize(path + f + '/message_1.json')
         print(size)
         if size > min_bytes:
