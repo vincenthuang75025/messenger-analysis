7c1ff439e0dbf027d25fdc964aee418954cc2b13
Marley <marleyxiong0@gmail.com>
2020-08-02 20:42:18 -0400

    stackplot

--- timestamp_analysis.py
+++ timestamp_analysis.py
@@ -2,6 +2,7 @@ import json
 from collections import Counter
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # load json file
 def load_json(path="timestamp_dict.json"):
@@ -26,4 +27,40 @@ print(day.head())
 def get_interaction(name, start_time,end_time,resolution):
 	return
 
-	
+day['timestamp']['Raffi Hotter'].plot()
+day['timestamp']['Zachary Feng'].plot()
+day['timestamp']['Chris Axon'].plot()
+
+names=['Raffi Hotter', 'Jasmine Wang','Frederick Zhang', 'Chris Axon', 'Zachary Feng', 'Jad Hamdan']
+for name in names:
+    data=day['timestamp'][name];plt.bar(data.index, data)
+    data['int_days'] = df['timestamp'].sub(pd.Timestamp('2018-01-01')).dt.days
+plt.show()
+def smooth(x,window_len=11,window='hanning'):
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+result = []
+for name in names:
+    data=day['timestamp'][name]
+    formatted = data.to_frame()
+    formatted['int_days'] = data.index.to_series().sub(pd.Timestamp('2018-01-01')).dt.days
+    bigarr = formatted.values
+    arr = np.zeros(365*2)
+    for i, num in bigarr:
+        arr[num-1] = i
+    result.append(smooth(arr,window_len=25))
+    # result.append(arr)
+plt.figure(figsize=(20,20))
+x = np.linspace(0,740, len(result[0]));
+plt.stackplot(x, result, labels=names)
+plt.legend()
+
