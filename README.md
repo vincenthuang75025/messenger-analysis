89b39b3278d5bd4232b6dd9873058b0b5a3a6e72
Marley <marleyxiong0@gmail.com>
2020-12-12 00:29:25 -0500

    cleanup, add instructions to README

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # README
 
-## Files
-*parse_messages.py* takes data form json and parses it into of dics for each person
-*convo_anal_1* for the moment it generate the does tfidf dictionary
+## Graph visualization
 
+![Screenshot](2019_2020.png)
+To make graph visualization,
+
+1. Download Messenger data from Facebook and move the downloaded `messages` folder into this folder
+2. run `python3 parse_messages_time_domain.py` to generate timestamp JSON
+3. run `python3 timestamp_analysis.py` to get pictures :)
+
+## Other files
+
+_parse_messages.py_ takes data form json and parses it into of dics for each person
+_convo_anal_1_ for the moment it generate the does tfidf dictionary
 
 ## Ideas
+
 - featurise each person / human readable insights for each person
-- featurise yourself per conversation 
+- featurise yourself per conversation
 - find online dictionary of expressions classified by something like affectio
 
-
 ### Quotes
-> When people are being deceptive in laboratory experiments, their use of first person singular drops significantly. Dr. James explains, "indeed, the use of 'I' is one of the best predictors of hoesty." Hoe much more can we learn by analyzing how a person speaks about themselves and the choice of words they use?
 
+> When people are being deceptive in laboratory experiments, their use of first person singular drops significantly. Dr. James explains, "indeed, the use of 'I' is one of the best predictors of hoesty." Hoe much more can we learn by analyzing how a person speaks about themselves and the choice of words they use?
--- timestamp_analysis.py
+++ timestamp_analysis.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 from datetime import datetime
-
 # load json file
-def load_json(path="timestamp_dict.json"):
-	with open(path) as json_file:
-		data = json.load(json_file)
-	return data
+
+MIN_MESSAGES = 20  # minimum number of messages for person to be included in plot
+OUTPUT_FILENAME = '2019_2020.png'
+start_day = '2019-01-01'  # can change this
+
+
+def load_json(path="timestamp_dict_3.json"):
+    with open(path) as json_file:
+        data = json.load(json_file)
+    return data
+
 
 data = load_json()
 
@@ -19,52 +25,38 @@ df = pd.DataFrame(data, columns=['name', 'timestamp'])
 df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 # Set the datetime column as the index
 df.index = df['timestamp']
-start_day = '2016-01-01' # can change this
 df = df[df['timestamp'] > start_day]
-min_date = df.index.min() 
+min_date = df.index.min()
 max_date = df.index.max()
 n_days = (max_date - min_date).days
 
 # group by name and day
 day = df.groupby([df['name'], pd.TimeGrouper(freq='D')]).count()
-print(df.head())
-print(day.head())
+# print(df.head())
+# print(day.head())
 
-# function that takes: name_of_frined , start_time, end_time , resoluction \in (hour / day / month)
-# returns 
-def get_interaction(name, start_time,end_time,resolution):
-	return
 
-day['timestamp']['Raffi Hotter'].plot()
-day['timestamp']['Zachary Feng'].plot()
-day['timestamp']['Chris Axon'].plot()
+def smooth(x, window_len=11, window='hanning'):
 
-names=['Raffi Hotter', 'Jasmine Wang','Frederick Zhang', 'Chris Axon', 'Zachary Feng', 'Jad Hamdan']
-for name in names:
-    data=day['timestamp'][name];plt.bar(data.index, data)
-    data['int_days'] = df['timestamp'].sub(pd.Timestamp('2018-01-01')).dt.days
-plt.show()
-
-def smooth(x,window_len=11,window='hanning'):
-
-    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
-    #print(len(s))
-    if window == 'flat': #moving average
-        w=np.ones(window_len,'d')
+    s = np.r_[x[window_len-1:0:-1], x, x[-2:-window_len-1:-1]]
+    # print(len(s))
+    if window == 'flat':  # moving average
+        w = np.ones(window_len, 'd')
     else:
-        w=eval('np.'+window+'(window_len)')
+        w = eval('np.'+window+'(window_len)')
 
-    y=np.convolve(w/w.sum(),s,mode='valid')
+    y = np.convolve(w/w.sum(), s, mode='valid')
     return y
+
 
 result = []
 names = list(df['name'].unique())
-names = [name for name in names if len(df[df['name'] ==name]) > 200] #and name != 'Zachary Feng' and name != 'Gleb Posobin']
+names = [name for name in names if len(df[df['name'] == name]) > MIN_MESSAGES]
 
 
 first = []
 for name in names:
-    data=day['timestamp'][name]
+    data = day['timestamp'][name]
     formatted = data.to_frame()
     formatted['int_days'] = data.index.to_series().sub(min_date).dt.days
     bigarr = formatted.values
@@ -73,7 +65,7 @@ for name in names:
         arr[num-1] = i
     first_day = np.min(bigarr.T[1])
     first.append(first_day)
-    result.append(smooth(arr,window_len=25))
+    result.append(smooth(arr, window_len=10))
     # result.append(arr)
 first = np.array(first)
 inds = first.argsort()
@@ -82,9 +74,10 @@ colorange = first.max() + 1
 viridis = cm.get_cmap('Spectral', colorange)
 cmap = viridis(range(colorange))
 
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(20, 20))
 # x = np.linspace(0,n_days, len(result[0]))
 x = [min_date + pd.DateOffset(i) for i in range(len(result[0]))]
-plt.stackplot(x, np.array(result)[inds], labels=np.array(names)[inds], colors=[cmap[idx] for idx in first[inds]] )
-plt.legend()
-plt.savefig('2016_2020.png')
+plt.stackplot(x, np.array(result)[inds], labels=np.array(names)[
+              inds], colors=[cmap[idx] for idx in first[inds]])
+plt.legend(loc='upper left')
+plt.savefig(OUTPUT_FILENAME)
